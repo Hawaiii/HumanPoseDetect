8a95c6973847bcb5bca39af832bdc6b32e34f5e2
mengxin6 <limengxin6@gmail.com>
2016-04-04 23:01:07 -0400

    position_to_map_layer.cu added

---
+++ caffe_new_layers/position_to_map_layer.cu
@@ -0,0 +1,25 @@
+#include <vector>
+
+#include "caffe/layers/position_to_map_layer.hpp"
+#include "caffe/util/math_functions.hpp"
+
+namespace caffe {
+
+template <typename Dtype>
+void PositionToMapLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) {
+      Forward_cpu(bottom, top);
+}
+
+template <typename Dtype>
+void PositionToMapLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+      Backward_cpu(top, propagate_down, bottom);
+}
+
+
+INSTANTIATE_LAYER_GPU_FUNCS(PositionToMapLayer);
+
+}  // namespace caffe
+~                                                                                     
+~  
