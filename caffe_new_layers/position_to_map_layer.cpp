5329bbe0bad4190b5b8dac9561f8fa9eec993b06
mengxin6 <limengxin6@gmail.com>
2016-04-16 17:25:01 -0400

    new layers works

--- caffe_new_layers/position_to_map_layer.cpp
+++ caffe_new_layers/position_to_map_layer.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <vector>
 #include <algorithm>
 #include <math.h>
@@ -10,13 +11,14 @@ namespace caffe {
 template <typename Dtype>
 void PositionToMapLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
   vector<int> top_shape;
-  CHECK(bottom[1]->count()%3 == 0);
-  const int channels = bottom[1]->count() / 3;  //the number of joints
+  CHECK(bottom[1]->channels()%3 == 0);
+  const int channels = bottom[1]->channels() / 3;  //the number of joints
   top_shape.push_back(bottom[0]->num());       //batch size
   top_shape.push_back(channels);
   top_shape.push_back(bottom[0]->height());    //image height
   top_shape.push_back(bottom[0]->width());     //image width
-  
+  //for (int i=0; i<top_shape.size();++i)
+  //std::cout<<top_shape[i]<<std::endl;
   top[0]->Reshape(top_shape);
 }
 
@@ -26,31 +28,37 @@ void PositionToMapLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom, 
   const int halfWindowSize = this->layer_param_.gaussian_param().half_window_size(); // windowSize=2*halfWindowSIze+1
   
   const Dtype* bottom_joint = bottom[1]->cpu_data();
+  const int joint_count = bottom[1]->count();
+  const int joint_num = bottom[1]->num();
   Dtype* top_data = top[0]->mutable_cpu_data();
   const int count = top[0]->count();       // count= batchSize*channels*width*height
   const int num = top[0]->num();           // get batch size
   const int channels = top[0]->channels(); // get number of channels = number of joints
   const int height = top[0] -> height(); // get image height
   const int width = top[0] -> width();   // get image width
+  
+  //std::cout<<1<<std::endl;
 
   for (int i = 0; i < count; ++i)
   {
     top_data[i]=0;
-  }
-   
+  } 
+
+  //std::cout<<2<<std::endl;
+ 
   for (int i=0; i<num; ++i)         // loop for images in a batch
   {
     for (int j=0; j<channels; ++j)   //loop for joints in an image
     {
-      bool visible = (bottom_joint[i*3+2]!=-1);
+      bool visible = (bottom_joint[i*(joint_count/joint_num)+j*3+2]!=-1);
       if (visible)
       {
         //assign values according to the joint location and gaussian value
-        int x=bottom_joint[i*3];   // NOTE: x indicates column and y indicates row 
-        int y=bottom_joint[i*3+1];   // Here I assume that x indicates column, starts from 1, and is the second value in joint txt, check with Hawaii
-        for (int h=std::min(y-halfWindowSize-1,0); h<std::max(y+halfWindowSize,height); ++h)
+        int x=bottom_joint[i*(joint_count/joint_num)+j*3];   // NOTE: x indicates column and y indicates row 
+        int y=bottom_joint[i*(joint_count/joint_num)+j*3+1];   // Here I assume that x indicates column, starts from 1, and is the second value in joint txt, check with Hawaii
+        for (int h=std::max(y-halfWindowSize-1,0); h<std::min(y+halfWindowSize,height); ++h)
         {
-           for (int w=std::min(x-halfWindowSize-1,0); w<std::max(x+halfWindowSize,width); ++w)   //loop for each element in gaussian window
+           for (int w=std::max(x-halfWindowSize-1,0); w<std::min(x+halfWindowSize,width); ++w)   //loop for each element in gaussian window
            {
               double value=255/(pow(sigma,2)*2*M_PI)*exp((-pow((w-x),2)-pow((h-y),2))/(2*pow(sigma,2))); 
               top_data[i*num+j*channels+h*height+w]=value;
@@ -59,6 +67,7 @@ void PositionToMapLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom, 
        }
      } 
    }
+   //std::cout<<3<<std::endl;
 }
 
 
